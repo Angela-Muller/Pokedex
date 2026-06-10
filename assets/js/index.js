057b490f26463103933f033d0038368fad2ee4cc
ANGELA MULLER <70184433428@senacrs.edu.br>
2026-06-10 10:07:16 -0300

    feat: audio play

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+const btn1 = document.getElementById('btn1')
+const btn2 = document.getElementById('btn2')
+const btnMusic = document.getElementById('btnMusic')
+
+btn1.addEventListener('click', () => {
+    return page // ./pages/pokedex.html
+})
+
+btn2.addEventListener('click', () => {
+    return page // ./pages/pokeBattle.html
+})
+
+btnMusic.addEventListener('click', () => {
+    Audio: new (src = "./assets/aud/poke.mpeg", type='audio/mpeg') = HTMLAudioElement
+})
